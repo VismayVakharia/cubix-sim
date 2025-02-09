1f4288e7a920bbf24ef8d4f24629351672c83ae3
Vismay Vakharia <VismayVakharia@users.noreply.github.com>
2025-02-09 22:28:58 +0530

    add github action workflow config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/cubix-sim',
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
